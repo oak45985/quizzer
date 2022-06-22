1d15b72b16f706a8399bb14cf84ecbe8da705e32
oak45985 <oak45985@gmail.com>
2022-06-21 21:57:11 -0500

    updated correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@ var timerEl = document.getElementById('countdown');
 var startQuizEl = document.querySelector("#start-button");
 var questionListEl = document.querySelector("#question-list");
 var createQuizEl = [];
-var qOne  = ['This is question 1', 'Answer 1.1', 'Answer 1.2', 'Answer 1.3', 'Answer 1.4'];
-var qTwo  = ['This is question 2', 'Answer 2.1', 'Answer 2.2', 'Answer 2.3', 'Answer 2.4'];
-var qThr  = ['This is question 3', 'Answer 3.1', 'Answer 3.2', 'Answer 3.3', 'Answer 3.4'];
-var qCycle = [];
+var ques  = [ { 
+        question: 'This is question 1', 
+        answers: { a: 'Answer 1.1', b: 'Answer 1.2', c: 'Answer 1.3', d: 'Answer 1.4'}, 
+        correctAnswer: 'a'},
+        { 
+        question: 'This is question 2', 
+        answers: { a: 'Answer 2.1', b: 'Answer 2.2', c: 'Answer 2.3', d: 'Answer 2.4'}, 
+        correctAnswer: 'c'},
+        { 
+        question: 'This is question 3', 
+        answers: { a: 'Answer 3.1', b: 'Answer 3.2', c: 'Answer 3.3', d: 'Answer 3.4'}, 
+        correctAnswer: 'c'}
+        ];
+var currentQ = 0;
 
 var killButton = function(event) {
     event.preventDefault();
@@ -16,48 +26,85 @@ var killButton = function(event) {
 
     countdown();
 
-    beginQuiz();
+    beginQuizEl();
 };
 
-var beginQuiz = function () {
+var beginQuizEl = function () {
+
+    // var clickAnswerEl = [];
 
     var newQuestionEl = document.createElement("h3");
     newQuestionEl.className = "quiz-title";
-    newQuestionEl.innerHTML = qOne[0]
+    newQuestionEl.innerHTML = ques[0].question;
     questionListEl.appendChild(newQuestionEl);
 
     var newAnswerOneEl = document.createElement("button");
     newAnswerOneEl.className = "quiz-question"
-    newAnswerOneEl.innerHTML = qOne[1]
+    newAnswerOneEl.innerHTML = ques[0].answers.a;
+    newAnswerOneEl.setAttribute("data-value","a");
+    newAnswerOneEl.addEventListener("click", checkAnswer);
     questionListEl.appendChild(newAnswerOneEl);
 
     var newAnswerTwoEl = document.createElement("button");
     newAnswerTwoEl.className = "quiz-question"
-    newAnswerTwoEl.innerHTML = qOne[2]
+    newAnswerTwoEl.innerHTML = ques[0].answers.b;
+    newAnswerTwoEl.setAttribute("data-value", "b");
+    newAnswerTwoEl.addEventListener("click", checkAnswer);
     questionListEl.appendChild(newAnswerTwoEl);
 
     var newAnswerThrEl = document.createElement("button");
     newAnswerThrEl.className = "quiz-question"
-    newAnswerThrEl.innerHTML = qOne[3]
+    newAnswerThrEl.innerHTML = ques[0].answers.c;
+    newAnswerThrEl.setAttribute("data-value","c");
+    newAnswerThrEl.addEventListener("click", checkAnswer);
     questionListEl.appendChild(newAnswerThrEl);
 
     var newAnswerFouEl = document.createElement("button");
     newAnswerFouEl.className = "quiz-question"
-    newAnswerFouEl.innerHTML = qOne[4]
+    newAnswerFouEl.innerHTML = ques[0].answers.d;
+    newAnswerFouEl.setAttribute("data-value","d");
+    newAnswerFouEl.addEventListener("click", checkAnswer);
     questionListEl.appendChild(newAnswerFouEl);
+
 };
 
+function checkAnswer(event) {
+    // console.log(this,"onclick",event);
+    
+    var userSelect = this.getAttribute("data-value")
+    if (userSelect === ques[currentQ].correctAnswer) {
+        console.log("correct")
+    } else {
+        console.log("wrong")
+    };
+};
 
-var cycleQuestion = function(event) {
+ var whichQuestion = function() {
 
-    var qTry = qOne + qTwo + qThr;
-    // for (var i = 0; i < qTry.length; i++) {
-        
-    // }
-    console.log(qTry);
+    
 
     
 };
+
+var showQuestion = function(){
+    var currentQuestion = quizQuestions[questionNumber];
+  
+    var title = document.getElementById('question-title');
+    title.textContent = currentQuestion.question
+  
+    choicesElement.innerHTML = '';
+  
+    currentQuestion.answers.forEach(function(option, index) {
+      var choiceButton = document.createElement('button')
+      choiceButton.setAttribute('value', option);
+      choiceButton.setAttribute('class', 'choice');
+      choiceButton.textContent = index + 1 + ':' + option;
+      //check if question is right or wrong
+      choiceButton.onclick = checkAnswer
+      choicesElement.appendChild(choiceButton);
+  
+    });
+  };
 
 // var createQuizEl = function(quizQuestion) {
 //     var document.createElement("div");
@@ -118,5 +165,6 @@ function countdown() {
 
 }
 
+
 startQuizEl.addEventListener('click', killButton);
-startQuizEl.addEventListener('click', cycleQuestion);
+// startQuizEl.addEventListener('click', cycleQuestion);
